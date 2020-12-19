d3ecce6647ce228e965ca53219cdb1d85ff8a07b
Najla <najla.seghaier@medtech.tn>
2020-12-19 15:25:47 +0100

    internships layout

--- src/app/internships/student-internships/student-internships.component.ts
+++ src/app/internships/student-internships/student-internships.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faSearch, faStar} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-student-internships',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentInternshipsComponent implements OnInit {
 
+  faStar = faStar;
+  faSearch = faSearch;
+
+  internships = [
+    {Title: 'Some Title', Domain: 'SE', Company: {Name: 'Microsoft', Description: 'ssssss', Logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/1024px-Microsoft_logo.svg.png'}, Description: 'Some description aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', Location: 'Tunis', Deadline: '12/12/2020', Duration: '3 months'},
+    {Title: 'Some Title', Domain: 'SE', Company: {Name: 'Orange TN', Description: 'ssssss', Logo: 'https://logoeps.com/wp-content/uploads/2011/06/orange-logo-vector.png'}, Description: 'Some description', Location: 'Tunis', Deadline: '12/12/2020', Duration: '3 months'},
+    {Title: 'Some Title', Domain: 'SE', Company: {Name: 'Orange TN', Description: 'ssssss', Logo: 'https://logoeps.com/wp-content/uploads/2011/06/orange-logo-vector.png'}, Description: 'Some description', Location: 'Tunis', Deadline: '12/12/2020', Duration: '3 months'},
+    {Title: 'Some Title', Domain: 'SE', Company: {Name: 'Orange TN', Description: 'ssssss', Logo: 'https://logoeps.com/wp-content/uploads/2011/06/orange-logo-vector.png'}, Description: 'Some description', Location: 'Tunis', Deadline: '12/12/2020', Duration: '3 months'},
+  ]
   constructor() { }
 
   ngOnInit() {
